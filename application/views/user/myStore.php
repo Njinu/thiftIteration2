7a55f6cc0739545c96159bb64922973d21ea513e
Njinu <njinukimani@gmail.com>
2014-12-21 00:00:50 +0200

    Highcharts for graphs

--- application/views/user/myStore.php
+++ application/views/user/myStore.php
@@ -204,15 +204,14 @@
             <!-- /.navbar-collapse -->
         </nav>
 
-        <div id="page-wrapper">
+        <div id="page-wrapper" style="padding-top:45px;">
 
             <div class="container-fluid">
 
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Dashboard <small>Statistics Overview</small>
+                        <h1 class="page-header">Thrift Shop</small>
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
@@ -226,8 +225,33 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="alert alert-info alert-dismissable">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <i class="fa fa-info-circle"></i>  <strong>Like SB Admin?</strong> Try out <a href="http://startbootstrap.com/template-overviews/sb-admin-2" class="alert-link">SB Admin 2</a> for additional features!
+                        
+              <?php echo form_open('user/createItem',array('id'=>'frm')) ?>
+
+              <div class="form-group">  
+                <input type="text" class="form-control" id="ItemName" name="ItemName" placeholder="Enter Item Name">
+              </div>
+
+              <div class="form-group"> 
+                <input type="text" class="form-control" id="ItemDescription"  name="ItemDescription" placeholder="Enter Item Description">
+              </div>
+
+              <div class="form-group"> 
+                <input type="text" class="form-control" id="ItemCategory"  name="ItemCategory" placeholder="Enter Item Category">
+              </div>
+
+              <div class="form-group"> 
+                <input type="number" class="form-control" id="ItemPrice" name="ItemPrice" placeholder="Enter Item Price">
+              </div>
+
+              <div class="form-group">
+                <label for="ItemPicture">File input</label>
+                <input type="file" id="ItemPicture">
+              </div>
+
+              <button  type="submit"  class="btn btn-default">Submit</button>
+            </form>
+              
                         </div>
                     </div>
                 </div>
